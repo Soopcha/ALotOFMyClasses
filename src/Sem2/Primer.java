38adbb43d78d1c3877ca7826b01ab6514cca65ff
Soopcha <lena.malykhina.04@gmail.com>
2023-02-17 12:46:10 +0300

    eheрр6

---
+++ src/Sem2/Primer.java
@@ -0,0 +1,9 @@
+package Sem2;
+
+public class Primer {
+    public static void main(String[] args) throws Exception {
+        Encapsulation p = new Encapsulation();
+        int x = p.getX(); //чтение св-ва
+        p.setX(30);       // запись св-ва
+    }
+}
