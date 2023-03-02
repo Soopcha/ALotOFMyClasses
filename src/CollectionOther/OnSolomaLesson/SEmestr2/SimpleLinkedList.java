31669186fe7539d1e6f2f79571c54d49adeb487e
Soopcha <lena.malykhina.04@gmail.com>
2023-03-02 23:22:55 +0300

    eheрр7

--- src/CollectionOther/OnSolomaLesson/SEmestr2/SimpleLinkedList.java
+++ src/CollectionOther/OnSolomaLesson/SEmestr2/SimpleLinkedList.java
@@ -1,51 +1,92 @@
 package CollectionOther.OnSolomaLesson.SEmestr2;
+
 /*
 класс для списка целиком - те тут будут те методы, которые мы хотим видеть операции в списке целиком
  */
 public class SimpleLinkedList<T> {
 
+    public static class SimpleLinkedListException extends Exception{ // написали ИМЕННО НАШЕ исключение, которое наследуется вообще от всех исключений
+        public SimpleLinkedListException(String massage){ //наш единственный конструктор тут
+            super(massage);//super - вызов конструктора предка
+        }
+    }
+
     private class SimpleLinkedListNode<T> { //внутренний класс
-        public T value;
+        public T value; // паблик чтобы не реализовать методы гет и сет, но тк всё равно к классу обращаться будем чисто из верхнего класса то инкапсуляция сохраняется
         public SimpleLinkedListNode<T> next;
 
-        public SimpleLinkedListNode(T value, SimpleLinkedListNode<T> next){
+        public SimpleLinkedListNode(T value, SimpleLinkedListNode<T> next) {
             this.value = value;
             this.next = next;
         }
-    }
-    public static addFirst(T value){ // value - перевод: значение
 
-    }
-
-    public static addLast(T value){
-
+        public SimpleLinkedListNode(T value) {
+            this(value, null);
+        }
     }
 
-    public T removeFirst(){
+    private SimpleLinkedListNode<T> head = null;  // тк класс без конструктора, то автоматом переменным присвоятся значения при инициализаци
+    private SimpleLinkedListNode<T> tail = null;
+    private int count = 0;
+
+    public void addFirst(T value) { // value - перевод: значение
+        // операция добавления эл в начало списка
+        head = new SimpleLinkedListNode<>(value);
+        if (count == 0){ // те если список был пустой
+            tail = head;
+        }
+        count++;
+    }
+
+    public void addLast(T value) {  // операция добавления эл в конец списка
+        SimpleLinkedListNode<T> temp = new SimpleLinkedListNode<>(value);
+        if(count == 0){ //если не было эл в списке
+            head = tail = temp;
+        } else {
+            tail.next = temp;
+            tail = temp;
+        }
+        count++;
+    }
+
+    private void checkEmpty() throws SimpleLinkedListException{ // если список пустой то выкидываем исключение, а метод этот специально для этого
+        if (count == 0){
+            throw new SimpleLinkedListException("Empty list");
+        }
+    }
+
+    public T removeFirst() throws SimpleLinkedListException { //удаление эл из начала списка(+ нужно исключение если вообще эл нет в списке)
+        checkEmpty();
+        T value  = head.value;;
+        head = head.next;
+        if (count == 1){
+            tail = null;
+        }
+        count--;
+        return value;
+    }
+
+    public T removeLast() {
 
     }
 
-    public T removeLast(){
+    public T remove() {
 
     }
 
-    public T remove(){
+    public T insert(int index, T value) { // вставить эл
 
     }
 
-    public T insert(int index, T value){ // вставить эл
+    public int size() {
 
     }
 
-    public  int size (){
+    public T getFirst() {
 
     }
 
-    public T getFirst(){
-
-    }
-
-    public T getLast(){
+    public T getLast() {
 
     }
 
